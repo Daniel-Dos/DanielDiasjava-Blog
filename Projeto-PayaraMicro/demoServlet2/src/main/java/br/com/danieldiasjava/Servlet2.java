9fbe374925ede9061cb799fed121fe329cb27bf3
daniel-dos <daniel.dias.analistati@gmail.com>
2018-06-02 20:39:45 -0300

    add demo 2

---
+++ Projeto-PayaraMicro/demoServlet2/src/main/java/br/com/danieldiasjava/Servlet2.java
@@ -0,0 +1,30 @@
+package br.com.danieldiasjava;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/HelloPayaraMicro2")
+public class Servlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet2() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("<h1>Multiplo Deploy</h1>");
+	}
+
+}
